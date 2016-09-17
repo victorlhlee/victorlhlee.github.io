c3f45eaf02969eba3ca176494d01327321fafd04
Victor <viclee02@gmail.com>
2016-09-16 17:05:54 -1000

    updated oct schedule

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var dates = [
   {"day": "Oct 8 (Sat)", "time": "14:00 - 16:00"},
   {"day": "Oct 15 (Sat)", "time": "14:00 - 16:00"},
   {"day": "Oct 22 (Sat)", "time": "9:00 - 12:00"},
-  {"day": "Sept 29 (Sat)", "time": "12:00 - 14:00"}
+  {"day": "Oct 29 (Sat)", "time": "12:00 - 14:00"}
 ];
 
 function playTime(schedule){
